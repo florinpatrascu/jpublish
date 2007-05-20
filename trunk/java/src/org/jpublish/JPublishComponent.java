3b30fad577fa119da48cf40d25fb2e483f6b7109
florin.patrascu <florin.patrascu@4b3360e8-6430-0410-8fa4-f3971a2249f0>
2007-05-20 19:07:42 +0000

    improved JPublish Component support. Ready for more complicated tasks, such as: adding Wiki support, etc :)
    Older Components must implement the new supporting methods.

--- trunk/java/src/org/jpublish/JPublishComponent.java
+++ trunk/java/src/org/jpublish/JPublishComponent.java
@@ -17,105 +17,138 @@
 
 package org.jpublish;
 
-import java.util.Map;
-
 import com.anthonyeden.lib.config.Configuration;
 import com.anthonyeden.lib.config.ConfigurationException;
-
 import org.jpublish.view.ViewRenderer;
 
-/** Standard interface which must be implemented by all JPublish components.
+import java.util.Map;
 
-    @author Anthony Eden
-    @since 2.0
-*/
+/**
+ * Standard interface which must be implemented by all JPublish components.
+ *
+ * @author Anthony Eden
+ * @author <a href="mailto:florin.patrascu@gmail.com">Florin T.PATRASCU</a>
+ * @since $Revision$ (updated: Sunday; May 20, 2007)
+ */
 
-public interface JPublishComponent{
-    
-    /** Get the component's name.
-    
-        @return The name
-    */
-    
+public interface JPublishComponent {
+
+    /**
+     * Get the component's name.
+     *
+     * @return The name
+     */
+
     public String getName();
-    
-    /** Get a description of the component.
-    
-        @return The component description
-    */
-    
+
+    /**
+     * Get a description of the component.
+     *
+     * @return The component description
+     */
+
     public String getDescription();
-    
-    /** Get a Map of component properties.
-    
-        @return A Map of properties
-    */
-    
+
+    /**
+     * Get a Map of component properties.
+     *
+     * @return A Map of properties
+     */
+
     public Map getProperties();
-    
-    /** Get the SiteContext.
-    
-        @return The SiteContext
-    */
-    
+
+    /**
+     * Get the SiteContext.
+     *
+     * @return The SiteContext
+     */
+
     public SiteContext getSiteContext();
-    
-    /** Set the SiteContext.
-    
-        @param siteContext The SiteContext
-    */
-    
+
+    /**
+     * Set the SiteContext.
+     *
+     * @param siteContext The SiteContext
+     */
+
     public void setSiteContext(SiteContext siteContext);
-    
-    /** Get the component's view repository.
-    
-        @return The view repository
-    */
-    
+
+    /**
+     * Get the component's view repository.
+     *
+     * @return The view repository
+     */
+
     public Repository getViewRepository();
-    
-    /** Set the component's view repository.
-    
-        @param viewRepository The new view repository
-    */
-    
+
+    /**
+     * Set the component's view repository.
+     *
+     * @param viewRepository The new view repository
+     */
+
     public void setViewRepository(Repository viewRepository);
-    
-    /** Get the component's view renderer.  This method may return
-        null which indicates that the component should use the default
-        view renderer specified in the SiteContext.
-    
-        @return The view renderer
-    */
-    
+
+    /**
+     * Get the component's view renderer.  This method may return
+     * null which indicates that the component should use the default
+     * view renderer specified in the SiteContext.
+     *
+     * @return The view renderer
+     */
+
     public ViewRenderer getViewRenderer();
-    
-    /** Set the component's view renderer.  Set the value to null
-        to indicate that the component should use the default
-        view renderer specified in the SiteContext.
-    
-        @param viewRenderer The new view renderer
-    */
-    
+
+    /**
+     * Set the component's view renderer.  Set the value to null
+     * to indicate that the component should use the default
+     * view renderer specified in the SiteContext.
+     *
+     * @param viewRenderer The new view renderer
+     */
+
     public void setViewRenderer(ViewRenderer viewRenderer);
-    
-    /** Load the component's configuration data.
-    
-        @param configuration The configuration data
-        @throws ConfigurationException
-    */
-    
+
+    /**
+     * Load the component's configuration data.
+     *
+     * @param configuration The configuration data
+     * @throws ConfigurationException
+     */
+
     public void loadConfiguration(Configuration configuration)
-    throws ConfigurationException;
-    
-    /** Render the component for the specified path and return the result.
-    
-        @param path The request path
-        @param context The context
-        @return The rendered component
-        @throws Exception
-    */
-    
+            throws ConfigurationException;
+
+    /**
+     * Render the component for the specified path and return the result.
+     *
+     * @param path    The request path
+     * @param context The context
+     * @return The rendered component
+     * @throws Exception
+     */
+
     public String render(String path, JPublishContext context) throws Exception;
-    
+
+    /**
+     * Process the text parameter and Render the component
+     *
+     * @param text    the string to process
+     * @param context The context
+     * @return The rendered component
+     * @throws Exception any Exception
+     */
+    public String renderText(String text, JPublishContext context) throws Exception;
+
+
+    /**
+     * Process the View from at the given path and Render the component
+     *
+     * @param path    the View to process
+     * @param context The context
+     * @return The rendered component
+     * @throws Exception any Exception
+     */
+    public String renderPath(String path, JPublishContext context) throws Exception;
+
 }
