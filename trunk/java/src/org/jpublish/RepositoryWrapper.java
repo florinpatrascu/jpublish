c168cf5ff3b2efdea9cded06cee31fee89ae902f
florin.patrascu <florin.patrascu@4b3360e8-6430-0410-8fa4-f3971a2249f0>
2007-08-25 14:39:45 +0000

    - added new rendering support through the StringTemplateViewRenderer. For   more details visit the JPublish Wiki. We believe that ST will become the   default template language for JPublish. - wrapped the RepositoryWrapper in a Map, so modern template engines will   have an easy job identifying the repository content resources as simple keys. - small optimization - preparing to branch the SVN

--- trunk/java/src/org/jpublish/RepositoryWrapper.java
+++ trunk/java/src/org/jpublish/RepositoryWrapper.java
@@ -1,40 +1,47 @@
 /*
- * Copyright 2004-2007 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+* Copyright 2004-2007 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
 
 package org.jpublish;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.*;
 
 /**
  * A wrapper around any repository.  This wrapper is used to expose the
  * Repository to the view renderer.  The current context is stored when
  * the wrapper is created which means that the context does not have to
- * be passed to the <code>get()</code> methods at request-time.
+ * be passed to the <code>get()</code> methods at request-time. Wrapping it
+ * in a Map for making easier the integration with modern template engines
+ * such as: StringTemplate. This is the discussion thread that made me decide
+ * about the implementation:
+ * http://hardlikesoftware.com/weblog/2007/06/25/thoughts-on-stringtemplate-part-2/#comment-1498
  *
  * @author Anthony Eden
  * @author <a href="mailto:florin.patrascu@gmail.com">Florin T.PATRASCU</a>
  */
 
-public class RepositoryWrapper {
+public class RepositoryWrapper implements Map {
+    public static final String EMPTY_STRING = "";
     private static final Log log = LogFactory.getLog(RepositoryWrapper.class);
 
     private Repository repository;
@@ -52,25 +59,17 @@ public class RepositoryWrapper {
         this.context = context;
     }
 
-    /**
-     * simple helper to avoid confusions during reflection
-     *
-     * @param path resource path that has to be extracted from the repository
-     * @return the String representation of the resource as processed by the Viewer
-     */
-    public String render(String path) {
-        return render(path, true);
-    }
 
     /**
-     * simple helper to avoid confusions during reflection
+     * Get the content at the given path.  Errors will be caught and the
+     * error message will be returned
+     * in place of the content.
      *
-     * @param path resource path that has to be extracted from the repository
-     * @return the String representation of the resource as processed by the Viewer
-     * @param merged True to merge the page context
+     * @param path The content path
+     * @return The content as a String
      */
-    public String render(String path, boolean merged) {
-        return get(path, merged);
+    public Object get(Object path) {
+        return this.get((String) path);
     }
 
     /**
@@ -80,7 +79,6 @@ public class RepositoryWrapper {
      * @param path The content path
      * @return The content as a String
      */
-
     public String get(String path) {
         return get(path, true);
     }
@@ -95,7 +93,6 @@ public class RepositoryWrapper {
      * @param merged True to merge
      * @return The content as a String
      */
-
     public String get(String path, boolean merged) {
         try {
             if (merged)
@@ -123,7 +120,7 @@ public class RepositoryWrapper {
      *
      * @param path The path to the content
      * @return The InputStream
-     * @throws Exception
+     * @throws Exception if errors occur during the acquisition of the inputstream
      */
     public InputStream getInputStream(String path) throws Exception {
         return repository.getInputStream(path);
@@ -136,11 +133,112 @@ public class RepositoryWrapper {
      *
      * @param path The path to the content
      * @return The OutputStream
-     * @throws Exception
+     * @throws Exception if errors occur during the acquisition of the outputstream
      */
-
     public OutputStream getOutputStream(String path) throws Exception {
         return repository.getOutputStream(path);
     }
 
+    /**
+     * @return the number of files/directories available in the repository path
+     */
+    public int size() {
+        File file = repository.pathToFile(EMPTY_STRING);
+        int size = 0;
+
+        if (file.isDirectory()) {
+            size = file.list().length;
+        }
+        return size;
+    }
+
+    /**
+     * @return true if the repository contains no files/directories
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * check if the repository contains the file/directory at the given path
+     *
+     * @param path the path to the content
+     * @return true if the file/directory is found, false otherwise
+     */
+    public boolean containsKey(Object path) {
+        return repository.pathToFile((String) path) != null;
+    }
+
+    /**
+     * todo: don't know yet the best behavior for this one.
+     *
+     * @param object ideally the pre-rendered content of a page. Tough one.
+     * @return false, for now
+     */
+    public boolean containsValue(Object object) {
+        //can't say :'(
+        //NYI
+        return false;
+    }
+
+
+    public Object put(Object key, Object value) {
+        //NYI
+        return null;
+    }
+
+    /**
+     * if implemented (uncommented), this will delete
+     * the content at the specified path!
+     *
+     * @param path the path to the file or the directory that will be removed
+     * @return null
+     */
+    public Object remove(Object path) {
+        //try {
+        // are you sure???
+        //ok then:
+        //todo: check if is file or directory, then
+        //repository.remove((String) key);
+        //} catch (Exception e) {
+        //    e.printStackTrace();
+        //}
+        log.warn("Are you sure you want to remove: " + path + "?????");
+        return null;
+    }
+
+    public void putAll(Map map) {
+        //NYI
+    }
+
+    public void clear() {
+        //NYI
+    }
+
+    /**
+     * Finds all the file/directories names in the repository
+     *
+     * @return a Set containing the file/directory names in the wrapped repository
+     */
+    public Set keySet() {
+        File file = repository.pathToFile(EMPTY_STRING);
+        Set keys = null;
+        try {
+            String[] filesInRepository = file.list();
+            if (filesInRepository != null && filesInRepository.length > 0) {
+                keys = new HashSet(Arrays.asList(filesInRepository));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return keys;
+    }
+
+    public Collection values() {
+        return null;
+    }
+
+    public Set entrySet() {
+        return null;
+    }
 }
