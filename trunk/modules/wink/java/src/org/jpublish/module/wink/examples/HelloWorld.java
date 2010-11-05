867b2afd90e56e8a492b796e7875ed7dbf6e0685
florin.patrascu@gmail.com <florin.patrascu@gmail.com@4b3360e8-6430-0410-8fa4-f3971a2249f0>
2010-11-05 00:29:01 +0000

    added a simple file upload example using Wink. Test: curl -H "Accept:text/plain" -F "name=@text.txt"  http://localhost:8080/wink/world/file/file_out

--- trunk/modules/wink/java/src/org/jpublish/module/wink/examples/HelloWorld.java
+++ trunk/modules/wink/java/src/org/jpublish/module/wink/examples/HelloWorld.java
@@ -1,13 +1,24 @@
 package org.jpublish.module.wink.examples;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.wink.common.internal.utils.MediaTypeUtils;
+import org.apache.wink.common.model.multipart.InMultiPart;
+import org.apache.wink.common.model.multipart.InPart;
 import org.apache.wink.common.model.synd.SyndEntry;
 import org.apache.wink.common.model.synd.SyndText;
+import org.jpublish.util.FileCopyUtils;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Simple example - Hello World!
@@ -35,13 +46,14 @@ import java.util.Date;
  * you get this:
  * <?xml version="1.0" encoding="utf-8" standalone="yes"?>
  * <entry xmlns:ns3="http://www.w3.org/1999/xhtml" xmlns:ns2="http://a9.com/-/spec/opensearch/1.1/" xmlns="http://www.w3.org/2005/Atom">
- *   <id>helloworld:1</id>
- *   <updated>2010-04-12T16:50:31.958-04:00</updated>
- *   <title type="text">Hello World!</title>
+ * <id>helloworld:1</id>
+ * <updated>2010-04-12T16:50:31.958-04:00</updated>
+ * <title type="text">Hello World!</title>
  * </entry>
  */
 @Path("/wink/world")
 public class HelloWorld {
+    protected static final Log log = LogFactory.getLog(HelloWorld.class);
     public static final String ID = "helloworld:1";
 
     /**
@@ -67,4 +79,50 @@ public class HelloWorld {
     public String getGreetingHtml() {
         return "Hello World!";
     }
+
+    /**
+     * demonstrating a multipart transfer (file upload) with Wink. Expects a file name as a
+     * path parameter. The file name will be used to create a temporary file. Example of usage:
+     *
+     * curl -H "Accept:text/plain" -F "name=@text.txt"  http://localhost:8080/wink/world/file/file_out
+     *
+     * @param inMultiPart the multipart form data
+     * @return a string confirming the file uploaded and its size
+     * @throws IOException
+     */
+
+    @Path("file/{fileName}")
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaTypeUtils.MULTIPART_FORM_DATA)
+    public String uploadFiles(InMultiPart inMultiPart, @PathParam("fileName") String fileName) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        while (inMultiPart.hasNext()) {
+            File f = File.createTempFile(fileName, ".tmp");
+            FileOutputStream fos = new FileOutputStream(f);
+
+            InPart part = inMultiPart.next();
+            MultivaluedMap<String, String> headers = part.getHeaders();
+
+            String CDHeader = headers.getFirst("Content-Disposition");
+            InputStream is = part.getInputStream();
+
+            int size = FileCopyUtils.copy(is, fos);
+
+            String fn = "";
+            Pattern p = Pattern.compile("filename=\".*\"");
+            Matcher m = p.matcher(CDHeader);
+
+            if (m.find()) {
+                fn = m.group();
+            }
+
+            //String filename = CDHeader.
+            sb.append("uploaded a file, ").append(fn).append(", size = ").append(size).append(" bytes\n");
+            log.info(String.format("%s, created", f.getAbsolutePath()));
+        }
+        return sb.toString();
+    }
+
 }
