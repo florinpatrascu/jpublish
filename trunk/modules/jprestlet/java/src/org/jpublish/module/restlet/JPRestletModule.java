d1238dec36040ebeb20c0669c80bf763298dacc5
florin.patrascu@gmail.com <florin.patrascu@gmail.com@4b3360e8-6430-0410-8fa4-f3971a2249f0>
2014-10-10 20:06:04 +0000

    let the user manage the response location ref by using a context variable

--- trunk/modules/jprestlet/java/src/org/jpublish/module/restlet/JPRestletModule.java
+++ trunk/modules/jprestlet/java/src/org/jpublish/module/restlet/JPRestletModule.java
@@ -32,144 +32,145 @@ import java.util.Map;
  * @since $Revision$ (created: 2008.10.31 9:24:41 PM)
  */
 public class JPRestletModule implements JPublishModule {
-    public static final String JPRESTLET_MODULE_TAG = "__JPRestletModule";
-    public static final String JPCONTEXT_RESTLET_TAG = "__JPRestletJPContext";
-    public static final String EMPTY_STRING = "";
+  public static final String JPRESTLET_MODULE_TAG = "__JPRestletModule";
+  public static final String JPCONTEXT_RESTLET_TAG = "__JPRestletJPContext";
+  public static final String EMPTY_STRING = "";
 
-    private static final Log log = LogFactory.getLog(JPRestletModule.class);
+  private static final Log log = LogFactory.getLog(JPRestletModule.class);
 
-    private static final String NAME = "JPRestlet Module";
-    private static final String VERSION = "1.0.2";
-    private static final String JPUBLISH_DEFAULT_REST_URL = "/rest";
+  private static final String NAME = "JPRestlet Module";
+  private static final String VERSION = "1.0.2";
+  private static final String JPUBLISH_DEFAULT_REST_URL = "/rest";
 
-    private static final String DESCRIPTION = "RESTLET (" + Engine.VERSION_HEADER + ") support for JPublish";
+  private static final String DESCRIPTION = "RESTLET (" + Engine.VERSION_HEADER + ") support for JPublish";
 
-    private SiteContext site;
-    private Map actions = new HashMap(5);
-    private ServletConverter converter;
-    public boolean debug = false;
-    private String restJPublishURL = JPUBLISH_DEFAULT_REST_URL;
-    private List restJPublishURLS = new ArrayList();
+  private SiteContext site;
+  private Map actions = new HashMap(5);
+  private ServletConverter converter;
+  public boolean debug = false;
+  private List<String> restJPublishURLS = new ArrayList<String>();
 
-    private String repository = "content";
-    public static final String JP_RESTLET_ACTION_RESPONSE = "__jpRestletActionResponseValue__";
-    public static final String JP_RESTLET_ACTION_RESPONSE_TYPE = "__jpRestletActionResponseType__";
-    private static final String RESTLET_TAG = "restlet";
-    private static final String RESOURCE_TAG = "resource";
+  private String repository = "content";
+  public static final String JP_RESTLET_ACTION_RESPONSE = "__jpRestletActionResponseValue__";
+  public static final String JP_RESTLET_ACTION_RESPONSE_TYPE = "__jpRestletActionResponseType__";
+  public static final String JPRESTLET_RESPONSE_STATUS = "jprestlet_response_status";
+  public static final String JPRESTLET_RESPONSE_LOCATION_REF = "jprestlet_response_location_ref";
 
-    public void init(SiteContext site, Configuration configuration) throws Exception {
-        log.info(NAME + " starting for: " + site.getServletContext().getServletContextName());
-        this.site = site;
+  private static final String RESTLET_TAG = "restlet";
+  private static final String RESOURCE_TAG = "resource";
 
-        String restletConfigPath = configuration.getChildValue("restlet-config", "WEB-INF/jprestlet-config.xml");
-        //legacy parameter
-        if (configuration.getChildValue("url") != null) {
-            restJPublishURLS.add(
-                    configuration.getChildValue("url", JPUBLISH_DEFAULT_REST_URL));
-        }
+  public void init(SiteContext site, Configuration configuration) throws Exception {
+    log.info(NAME + " starting for: " + site.getServletContext().getServletContextName());
+    this.site = site;
 
-        if (configuration.getChild("urls") != null) {
-            List restURLSConfig = configuration.getChild("urls").getChildren();
-            for (int i = 0; restURLSConfig != null && i < restURLSConfig.size(); i++) {
-                Configuration urlConfig = (Configuration) restURLSConfig.get(i);
-                restJPublishURLS.add(urlConfig.getValue(JPUBLISH_DEFAULT_REST_URL));
-            }
-        }
-
-        log.info("Initializing the " + this.toString() + " framework from: " + restletConfigPath);
-        this.converter = new ServletConverter(site.getServletContext());
-        initJPRestlet(restletConfigPath);
-
-        log.info("Mapping JPRestlet for all calls on: ");
-
-        //map JPRestletModule as a pth-action
-        for (int i = 0; i < restJPublishURLS.size(); i++) {
-            String url = (String) restJPublishURLS.get(i);
-            site.getActionManager().getPathActions().add(
-                    new ActionWrapper(
-                            new PathAction(url, new JPRestletAction(this)), configuration)
-            );
-            log.info("    " + url);
-        }
-
-        log.info(this.toString() + " is ready.");
+    String restletConfigPath = configuration.getChildValue("restlet-config", "WEB-INF/jprestlet-config.xml");
+    //legacy parameter
+    if (configuration.getChildValue("url") != null) {
+      restJPublishURLS.add(
+          configuration.getChildValue("url", JPUBLISH_DEFAULT_REST_URL));
     }
 
-    public Map getDefinedActions() {
-        return actions;
+    if (configuration.getChild("urls") != null) {
+      List restURLSConfig = configuration.getChild("urls").getChildren();
+      for (int i = 0; restURLSConfig != null && i < restURLSConfig.size(); i++) {
+        Configuration urlConfig = (Configuration) restURLSConfig.get(i);
+        restJPublishURLS.add(urlConfig.getValue(JPUBLISH_DEFAULT_REST_URL));
+      }
     }
 
-    public void destroy() {
-        log.info(this.toString() + ", destroyed.");
+    log.info("Initializing the " + this.toString() + " framework from: " + restletConfigPath);
+    this.converter = new ServletConverter(site.getServletContext());
+    initJPRestlet(restletConfigPath);
+
+    log.info("Mapping JPRestlet for all calls on: ");
+
+    //map JPRestletModule as a pth-action
+    for (String url : restJPublishURLS) {
+      site.getActionManager().getPathActions().add(
+          new ActionWrapper(
+              new PathAction(url, new JPRestletAction(this)), configuration)
+      );
+      log.info("    " + url);
     }
 
-    private void initJPRestlet(String restletConfigurationPath) throws Exception {
-        Context restletContext = converter.getContext();
-        Router router = new Router(restletContext);
-        File restletConfigurationFile = new File(site.getRoot(), restletConfigurationPath);
-        Configuration restletConfiguration = new XMLConfiguration("restletConfiguration", restletConfigurationFile);
+    log.info(this.toString() + " is ready.");
+  }
 
-        List routes = restletConfiguration.getChild("routes").getChildren();
-        repository = restletConfiguration.getChild("repository").getAttribute("name", "content");
-        log.info("Default restlet repository: " + repository);
-        log.info("Adding routes to RESTLET support...");
-        if (routes != null && !routes.isEmpty()) {
-            for (int j = 0; j < restJPublishURLS.size(); j++) {
-                String restJPublishURL = (String) restJPublishURLS.get(j);
-                String jpublishRestletPathPrefix = restJPublishURL.substring(0, restJPublishURL.lastIndexOf("/"));
-                for (int i = 0; i < routes.size(); i++) {
-                    Configuration route = (Configuration) routes.get(i);
-                    String map = route.getAttribute("map");
+  public Map getDefinedActions() {
+    return actions;
+  }
 
-                    if (map != null) {
-                        String jpRestletPath = jpublishRestletPathPrefix + map;
-                        String action = route.getAttribute("action", EMPTY_STRING);
-                        String page = route.getAttribute("page", EMPTY_STRING);
+  public void destroy() {
+    log.info(this.toString() + ", destroyed.");
+  }
 
-                        if (route.getAttribute(RESTLET_TAG) != null) {
-                            Restlet restlet = (Restlet) ClassUtilities.loadClass(route.getAttribute(RESTLET_TAG)).newInstance();
-                            router.attach(jpRestletPath, restlet);
-                            log.info("... restlet routing: " + jpRestletPath + " to: " + restlet);
-                        } else {
-                            final String resourceName = route.getAttribute(RESOURCE_TAG);
-                            if (resourceName != null) {
-                                router.attach(jpRestletPath, ClassUtilities.loadClass(resourceName));
-                                log.info("... resource routing: " + jpRestletPath + " to: " + resourceName);
-                            } else {
-                                router.attach(jpRestletPath, new JPSimpleRestlet(this, page, action));
-                                log.info("... action routing: " + jpRestletPath + " to action: " + action + ", page: " + page);
-                            }
-                        }
+  private void initJPRestlet(String restletConfigurationPath) throws Exception {
+    Context restletContext = converter.getContext();
+    Router router = new Router(restletContext);
+    File restletConfigurationFile = new File(site.getRoot(), restletConfigurationPath);
+    Configuration restletConfiguration = new XMLConfiguration("restletConfiguration", restletConfigurationFile);
 
-                    }
-                }
+    List routes = restletConfiguration.getChild("routes").getChildren();
+    repository = restletConfiguration.getChild("repository").getAttribute("name", "content");
+    log.info("Default restlet repository: " + repository);
+    log.info("Adding routes to RESTLET support...");
+    if (routes != null && !routes.isEmpty()) {
+      for (int j = 0; j < restJPublishURLS.size(); j++) {
+        String restJPublishURL = (String) restJPublishURLS.get(j);
+        String jpublishRestletPathPrefix = restJPublishURL.substring(0, restJPublishURL.lastIndexOf("/"));
+        for (int i = 0; i < routes.size(); i++) {
+          Configuration route = (Configuration) routes.get(i);
+          String map = route.getAttribute("map");
+
+          if (map != null) {
+            String jpRestletPath = jpublishRestletPathPrefix + map;
+            String action = route.getAttribute("action", EMPTY_STRING);
+            String page = route.getAttribute("page", EMPTY_STRING);
+
+            if (route.getAttribute(RESTLET_TAG) != null) {
+              Restlet restlet = (Restlet) ClassUtilities.loadClass(route.getAttribute(RESTLET_TAG)).newInstance();
+              router.attach(jpRestletPath, restlet);
+              log.info("... restlet routing: " + jpRestletPath + " to: " + restlet);
+            } else {
+              final String resourceName = route.getAttribute(RESOURCE_TAG);
+              if (resourceName != null) {
+                router.attach(jpRestletPath, ClassUtilities.loadClass(resourceName));
+                log.info("... resource routing: " + jpRestletPath + " to: " + resourceName);
+              } else {
+                router.attach(jpRestletPath, new JPSimpleRestlet(this, page, action));
+                log.info("... action routing: " + jpRestletPath + " to action: " + action + ", page: " + page);
+              }
             }
 
-        } else {
-            log.info(" ... no REST mappings? REST support will be unavailable for some of the web requests.");
-
+          }
         }
-        debug = restletConfiguration.getChildValue("debug", "false").equalsIgnoreCase("true");
-        converter.setTarget(router);
-    }
+      }
 
-    public String toString() {
-        return NAME + " version: " + VERSION + ", description: " + DESCRIPTION;
-    }
+    } else {
+      log.info(" ... no REST mappings? REST support will be unavailable for some of the web requests.");
 
-    public boolean isDebug() {
-        return debug;
     }
+    debug = restletConfiguration.getChildValue("debug", "false").equalsIgnoreCase("true");
+    converter.setTarget(router);
+  }
 
-    public ServletConverter getConverter() {
-        return converter;
-    }
+  public String toString() {
+    return NAME + " version: " + VERSION + ", description: " + DESCRIPTION;
+  }
 
-    public String getRepository() {
-        return repository;
-    }
+  public boolean isDebug() {
+    return debug;
+  }
 
-    public SiteContext getSite() {
-        return site;
-    }
+  public ServletConverter getConverter() {
+    return converter;
+  }
+
+  public String getRepository() {
+    return repository;
+  }
+
+  public SiteContext getSite() {
+    return site;
+  }
 }
--- trunk/modules/jprestlet/java/src/org/jpublish/module/restlet/JPSimpleRestlet.java
+++ trunk/modules/jprestlet/java/src/org/jpublish/module/restlet/JPSimpleRestlet.java
@@ -23,91 +23,105 @@ import java.util.Map;
  * @since $Revision$ (created: Mar 13, 2009 10:06:10 AM)
  */
 public class JPSimpleRestlet extends Restlet {
-    public static final Log log = LogFactory.getLog(JPSimpleRestlet.class);
-    private JPRestletModule module;
-    private String path;
-    private String action;
+  public static final Log log = LogFactory.getLog(JPSimpleRestlet.class);
+  private JPRestletModule module;
+  private String path;
+  private String action;
 
-    public JPSimpleRestlet(JPRestletModule module, String path, String action) {
-        this.module = module;
-        this.path = path;
-        this.action = action;
-    }
+  public JPSimpleRestlet(JPRestletModule module, String path, String action) {
+    this.module = module;
+    this.path = path;
+    this.action = action;
+  }
 
-    public void handle(Request request, Response response) {
-        JPublishContext context = (JPublishContext) request.getAttributes().get(JPRestletModule.JPCONTEXT_RESTLET_TAG);
-        MediaType valueType = MediaType.TEXT_PLAIN;
-        String value = JPRestletModule.EMPTY_STRING;
-        boolean actionExecuted = false;
-        boolean actionOnly = true;
+  public void handle(Request request, Response response) {
+    JPublishContext context = (JPublishContext) request.getAttributes().get(JPRestletModule.JPCONTEXT_RESTLET_TAG);
+    MediaType valueType = MediaType.TEXT_PLAIN;
+    String value = JPRestletModule.EMPTY_STRING;
 
-        if (context != null) {
+    boolean actionExecuted = false;
+    boolean actionOnly = true;
 
-            CharacterEncodingMap characterEncodingMap =
-                    module.getSite().getCharacterEncodingManager().getMap(path);
-            //todo decode the input parameters
-            //URLDecoder.decode("", characterEncodingMap.getRequestEncoding());
+    if (context != null) {
 
-            try {
-                Map reqAttributes = request.getAttributes();
-                if (reqAttributes != null && !reqAttributes.isEmpty()) {
-                    Iterator attributes = reqAttributes.keySet().iterator();
-                    // keep Java 1.4 for now :(
-                    while (attributes.hasNext()) {
-                        String attribute = (String) attributes.next();
-                        context.put(attribute, reqAttributes.get(attribute));
-                    }
-                }
+      CharacterEncodingMap characterEncodingMap =
+          module.getSite().getCharacterEncodingManager().getMap(path);
+      //todo decode the input parameters
+      //URLDecoder.decode("", characterEncodingMap.getRequestEncoding());
 
-                // 1st version; 2009.03.12
-                // is just an action we need?
-                if (!action.equals(JPRestletModule.EMPTY_STRING)) {
-                    module.getSite().getActionManager().execute(action, context);
-                    actionExecuted = true;
-                }
-
-                //or is it a page too?
-                if (!path.equals(JPRestletModule.EMPTY_STRING)) {
-                    actionOnly = false;
-                    // render the page at the given path
-                    value = module.getSite().getRepository(module.getRepository()).get(path, context);
-                }
-
-                if (actionExecuted && actionOnly) {
-                    value = (String) context.get(JPRestletModule.JP_RESTLET_ACTION_RESPONSE);
-                    //poor man validation (for now)
-                    if (value == null) {
-                        value = JPRestletModule.EMPTY_STRING;
-                    }
-                }
-
-                if (context.get(JPRestletModule.JP_RESTLET_ACTION_RESPONSE_TYPE) != null) {
-                    valueType = MediaType.valueOf(
-                            (String) context.get(JPRestletModule.JP_RESTLET_ACTION_RESPONSE_TYPE));
-                }
-
-                Representation r = new StringRepresentation(value, valueType);
-                //r.setCharacterSet(CharacterSet.UTF_8);
-                r.setCharacterSet(CharacterSet.valueOf(characterEncodingMap.getResponseEncoding()));
-                response.setEntity(r);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                response.setStatus(Status.CONNECTOR_ERROR_INTERNAL, e.getMessage());
-            }
+      try {
+        Map reqAttributes = request.getAttributes();
+        if (reqAttributes != null && !reqAttributes.isEmpty()) {
+          Iterator attributes = reqAttributes.keySet().iterator();
+          // keep Java 1.4 for now :(
+          while (attributes.hasNext()) {
+            String attribute = (String) attributes.next();
+            context.put(attribute, reqAttributes.get(attribute));
+          }
         }
+
+        // 1st version; 2009.03.12
+        // is just an action we need?
+        if (!action.equals(JPRestletModule.EMPTY_STRING)) {
+          module.getSite().getActionManager().execute(action, context);
+          actionExecuted = true;
+        }
+
+        //or is it a page too?
+        if (!path.equals(JPRestletModule.EMPTY_STRING)) {
+          actionOnly = false;
+          // render the page at the given path
+          value = module.getSite().getRepository(module.getRepository()).get(path, context);
+        }
+
+        if (actionExecuted && actionOnly) {
+          value = (String) context.get(JPRestletModule.JP_RESTLET_ACTION_RESPONSE);
+          //poor man validation (for now)
+          if (value == null) {
+            value = JPRestletModule.EMPTY_STRING;
+          }
+        }
+
+        if (context.get(JPRestletModule.JP_RESTLET_ACTION_RESPONSE_TYPE) != null) {
+          valueType = MediaType.valueOf(
+              (String) context.get(JPRestletModule.JP_RESTLET_ACTION_RESPONSE_TYPE));
+        }
+
+        Representation r = new StringRepresentation(value, valueType);
+        //r.setCharacterSet(CharacterSet.UTF_8);
+
+        if (context.get(JPRestletModule.JPRESTLET_RESPONSE_STATUS) != null) {
+          int status = (Integer) context.get(JPRestletModule.JPRESTLET_RESPONSE_STATUS);
+          if (status > 0) {
+            response.setStatus(new Status(status));
+          }
+        }
+
+        String locationRef = (String) context.get(JPRestletModule.JPRESTLET_RESPONSE_LOCATION_REF);
+        if (locationRef != null) {
+          response.setLocationRef(locationRef);
+        }
+
+        r.setCharacterSet(CharacterSet.valueOf(characterEncodingMap.getResponseEncoding()));
+        response.setEntity(r);
+
+      } catch (Exception e) {
+        e.printStackTrace();
+        response.setStatus(Status.CONNECTOR_ERROR_INTERNAL, e.getMessage());
+      }
     }
+  }
 
 
-    public static Log getLog() {
-        return log;
-    }
+  public static Log getLog() {
+    return log;
+  }
 
-    public String getPath() {
-        return path;
-    }
+  public String getPath() {
+    return path;
+  }
 
-    public String getAction() {
-        return action;
-    }
+  public String getAction() {
+    return action;
+  }
 }
